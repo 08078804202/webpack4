663bd796deff593baac1ed4555a2742edf23a68a
08078804202 <42214279+08078804202@users.noreply.github.com>
2024-01-31 00:52:56 +0900

    Update webpack.config copy.js

--- webpack.config copy.js
+++ webpack.config copy.js
@@ -135,7 +135,7 @@ module.exports = {
             directory: path.join(__dirname, 'public'),
         },
         compress: true,
-        port: 9000,
+        port: 3000,
         client: {
             overlay: true,
         },
